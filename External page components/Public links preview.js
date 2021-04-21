a5b5d31e718777063506d431c92db77c10b02072
Rob Habraken <roberthabraken@hotmail.com>
2021-04-21 23:06:16 +0200

    Refactor public link preview mock to only load once and add some basic styling (still mock code)

--- External page components/Public links preview.js
+++ External page components/Public links preview.js
@@ -1,47 +1,63 @@
 var self = this;
-self.pl = null;
+self.plo = null;
 
-options.mediator.subscribe("entityLoaded", function(entity) {
-    var rawJSON = JSON.stringify(entity);
-    var entityObject = JSON.parse(rawJSON);
-    var assetId = entityObject["id"];
-
-    document.getElementById("publicLinksPreviewGrid").innerHTML = "";
-
-    // parse AssetToPublicLink data to retrieve the entity IDs of the public links of this asset
-    $.getJSON("https://playground.stylelabs.io/api/entities/" + assetId + "/relations/AssetToPublicLink", function(data) { 
-        var publicLinkEntities = data["children"];
-        if(publicLinkEntities) {
-            for (var index = 0; index < publicLinkEntities.length; index++) {
-                var publicLinkAssetHref = publicLinkEntities[index]["href"];
-                if(publicLinkAssetHref && publicLinkAssetHref !== "") {
-                    self.pl = new PublicLinkDefinition();
-                    self.pl.initialize(publicLinkAssetHref);
-                }
-            }
-        }
-    });
-
+var entityLoadedSubscription = options.mediator.subscribe("entityLoaded", function (entity) {
+    if(self.plo == null) {
+        self.plo = new PublicLinkOverview();
+        self.plo.initialize(entity);
+    }
 });
 
-PublicLinkDefinition = function () {
-    // this._focalPoint = {};
+var entityUnloadedSubscription = options.mediator.subscribe("entityUnloaded", function (entity) {
+    self.plo.dispose();
+});
+
+PublicLinkOverview = function () {
+    this._item = null;
 }
 
-PublicLinkDefinition.prototype = {
-    initialize: function (href) {
+PublicLinkOverview.prototype = {
+    initialize: function (entity) {
+        this._item = entity;
+        this._test = 5;
 
-        $.getJSON(href, function(data) { 
-
-            var publicLink = data["public_link"];
-            if(publicLink && publicLink !== "") {
-                document.getElementById("publicLinksPreviewGrid").innerHTML += "<img src='" + publicLink + "&t=thumbnail&q=" + Math.random() + "' />";
-            }
-        });
-
+        this._getPublicLinkList();        
     },
 
     dispose: function () {
 
+    },
+
+    _getPublicLinkList: function () {
+        var rawJSON = JSON.stringify(this._item);
+        var entityObject = JSON.parse(rawJSON);
+        var assetId = entityObject["id"];
+
+        document.getElementById("publicLinksPreviewGrid").innerHTML = "";
+
+        // parse AssetToPublicLink data to retrieve the entity IDs of the public links of this asset
+        $.getJSON("https://playground.stylelabs.io/api/entities/" + assetId + "/relations/AssetToPublicLink", function(data) { 
+            var publicLinkEntities = data["children"];
+            if (publicLinkEntities) {
+                for (var index = 0; index < publicLinkEntities.length; index++) {
+                    var publicLinkAssetHref = publicLinkEntities[index]["href"];
+                    if(publicLinkAssetHref && publicLinkAssetHref !== "") {
+
+                        // retrieve the entity data of each public link and get the public link url to add a thumbnail to the overview
+                        $.getJSON(publicLinkAssetHref, function(publicLinkData) { 
+                            var publicLink = publicLinkData["public_link"];
+                            if (publicLink && publicLink !== "") {
+
+                                // the thumbnail is displayed using a transformation to avoid loading large images
+                                // to ensure the public link thumbnail isn't cached, we add a random value to the image source
+                                var html = "<div style='height:160px;'><img src='" + publicLink + "&t=thumbnail&r=" + Math.random() + "' width='50%' /><div class='micro-copy no-margin-bottom'>000 x 000 px</div></div>";
+                                document.getElementById("publicLinksPreviewGrid").innerHTML += html;
+                            }
+                        });
+
+                    }
+                }
+            }
+        });
     }
 }
