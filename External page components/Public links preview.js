6d457ba80b9c034fe9de07ab57c8cec82d235ce8
Ronald van der Plas <r.vdplas@quicknet.nl>
2021-12-20 21:44:24 +0100

    Added sorted Public Links order.

--- External page components/Public links preview.js
+++ External page components/Public links preview.js
@@ -81,6 +81,8 @@ PublicLinkOverview.prototype = {
             var publicLinkEntities = data["children"];
             if (publicLinkEntities) {
 
+                var AJAX = [];
+
                 var context = {itemCount: publicLinkEntities.length};
                 var html = headerTemplate(context);
                 document.getElementById("publicLinksHeader").innerHTML = html;
@@ -96,58 +98,83 @@ PublicLinkOverview.prototype = {
                 // iterate over the public link entities, retrieve their data and render the preview
                 for (var index = 0; index < publicLinkEntities.length; index++) {
                     var publicLinkAssetHref = publicLinkEntities[index]["href"];
-                    if(publicLinkAssetHref && publicLinkAssetHref !== "") {
-
-                        // retrieve the entity data of each public link and get the public link url to add a thumbnail to the overview
-                        $.getJSON(publicLinkAssetHref, function(publicLinkData) {
-                            var publicLink = publicLinkData["public_link"];
-                            if (publicLink && publicLink !== "") {
-                                
-                                // the thumbnail is displayed using a transformation to avoid loading large images
-                                // to ensure the public link thumbnail isn't cached, we add a random value to the image source
-                                var thumbnail = "";
-                                if (self.plo._renditions[publicLinkData["properties"]["Resource"]].content_type.startsWith("image")) {
-                                    // the thumbnail is only displayed for image type renditions (not for Documents for example, or other application type public links)
-                                    thumbnail = publicLink + "&t=thumbnail&r=" + Math.random();
-                                }
-
-                                var title = publicLinkData["properties"]["RelativeUrl"];
-                                var rendition = self.plo._renditions[publicLinkData["properties"]["Resource"]].label;
-                                var width = "";
-                                var height = "";
-
-                                var croppingType = null;
-                                if (publicLinkData["properties"]["ConversionConfiguration"] && publicLinkData["properties"]["ConversionConfiguration"]["cropping_configuration"]) {
-                                    croppingType = publicLinkData["properties"]["ConversionConfiguration"]["cropping_configuration"]["cropping_type"];
-                                    width = publicLinkData["properties"]["ConversionConfiguration"]["width"];
-                                    height = publicLinkData["properties"]["ConversionConfiguration"]["height"];
-
-                                    // depending on the way of cropping, the width and height are stored in different objects
-                                    if (!width || width == null) {
-                                        width = publicLinkData["properties"]["ConversionConfiguration"]["cropping_configuration"]["width"];
-                                        height = publicLinkData["properties"]["ConversionConfiguration"]["cropping_configuration"]["height"];
-                                    }
-                                }
-
-                                if (croppingType === "Entropy") {
-                                    croppingType = "Smart crop";
-                                } else if (croppingType === "Custom") {
-                                    croppingType = "Custom crop";
-                                } else if (croppingType === "CentralFocalPoint") {
-                                    croppingType = "Crop to center";
-                                } else {
-                                    croppingType = "Uncropped";
-                                }
-
-                                var context = {title: title, href: publicLink, preview: thumbnail, rendition: rendition, width: width, height: height, croppingType: croppingType};
-                                var html = listItemTemplate(context);
-                                document.getElementById("publicLinkList").innerHTML += html;
-                            }
-                        });
-
+                    if (publicLinkAssetHref && publicLinkAssetHref !== "") {
+                        AJAX.push($.getJSON(publicLinkAssetHref));
                     }
                 }
+
+                $.when.apply($, AJAX).done(function () {
+                    // This callback will be called with multiple arguments,
+                    // one for each AJAX call
+                    // Each argument is an array with the following structure: [data, statusText, jqXHR]
+
+                    // Let's map the arguments into an object, for ease of use
+                    var responses = [];
+                    for (var i = 0, len = arguments.length; i < len; i++) {
+                        responses.push(arguments[i][0]);
+                    }
+
+                    responses = responses.sort(compareResponse);
+                    responses.forEach(element => {
+                        let html = convertToHtml(element);
+                        document.getElementById("publicLinkList").innerHTML += html;
+                    });
+                });
             }
         }.bind(self));
+    }
+}
+
+function compareResponse(first, second) {
+    var firstTitle = first["properties"]["RelativeUrl"];
+    var secondTitle = second["properties"]["RelativeUrl"];
+
+    return ('' + firstTitle).localeCompare(secondTitle, undefined, { numeric: true, sensitivity: 'base' });
+}
+
+function convertToHtml(publicLinkData) {
+    // retrieve the entity data of each public link and get the public link url to add a thumbnail to the overview
+    var publicLink = publicLinkData["public_link"];
+    if (publicLink && publicLink !== "") {
+
+        // the thumbnail is displayed using a transformation to avoid loading large images
+        // to ensure the public link thumbnail isn't cached, we add a random value to the image source
+        var thumbnail = "";
+        if (self.plo._renditions[publicLinkData["properties"]["Resource"]].content_type.startsWith("image")) {
+            // the thumbnail is only displayed for image type renditions (not for Documents for example, or other application type public links)
+            thumbnail = publicLink + "&t=thumbnail&r=" + Math.random();
+        }
+
+        var title = publicLinkData["properties"]["RelativeUrl"];
+        var rendition = self.plo._renditions[publicLinkData["properties"]["Resource"]].label;
+        var width = "";
+        var height = "";
+
+        var croppingType = null;
+        if (publicLinkData["properties"]["ConversionConfiguration"] && publicLinkData["properties"]["ConversionConfiguration"]["cropping_configuration"]) {
+            croppingType = publicLinkData["properties"]["ConversionConfiguration"]["cropping_configuration"]["cropping_type"];
+            width = publicLinkData["properties"]["ConversionConfiguration"]["width"];
+            height = publicLinkData["properties"]["ConversionConfiguration"]["height"];
+
+            // depending on the way of cropping, the width and height are stored in different objects
+            if (!width || width == null) {
+                width = publicLinkData["properties"]["ConversionConfiguration"]["cropping_configuration"]["width"];
+                height = publicLinkData["properties"]["ConversionConfiguration"]["cropping_configuration"]["height"];
+            }
+        }
+
+        if (croppingType === "Entropy") {
+            croppingType = "Smart crop";
+        } else if (croppingType === "Custom") {
+            croppingType = "Custom crop";
+        } else if (croppingType === "CentralFocalPoint") {
+            croppingType = "Crop to center";
+        } else {
+            croppingType = "Uncropped";
+        }
+
+        var context = { title: title, href: publicLink, preview: thumbnail, rendition: rendition, width: width, height: height, croppingType: croppingType };
+        var html = listItemTemplate(context);
+        document.getElementById("publicLinkList").innerHTML += html;
     }
 }
