6ddd3224566028ad66dfebe0a9de2c2142aad0c8
Rob Habraken <roberthabraken@hotmail.com>
2023-02-13 22:18:42 +0100

    Simplify error boundary

--- React page components/src/Focal Point Cropping/ErrorBoundary.tsx
+++ React page components/src/Focal Point Cropping/ErrorBoundary.tsx
@@ -1,19 +1,19 @@
 import React, { Component, ErrorInfo, ReactNode } from "react";
 
-interface ErrorBoundaryProps {
+interface Props {
   children?: ReactNode;
 }
 
-interface ErrorBoundaryState {
+interface State {
   hasError: boolean;
 }
 
-class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
-  public state: ErrorBoundaryState = {
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
     hasError: false
   };
 
-  public static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+  public static getDerivedStateFromError(_: Error): State {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
@@ -24,7 +24,7 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
 
   public render() {
     if (this.state.hasError) {
-      return <h1>Sorry.. there was an error</h1>;
+      return <h1>Something went wrong</h1>;
     }
 
     return this.props.children;
